b7a9adbee7a7c0dfbe9220013b5d9bcc6c08951f
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-23 12:42:28 +0200

    Angular y los hooks

--- Teoria-Angular/src/app/videojuego/videojuego.component.ts
+++ Teoria-Angular/src/app/videojuego/videojuego.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, DoCheck, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'videojuego',
   templateUrl: './videojuego.component.html'
 
 })
-export class VideojuegoComponent {
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
   public titulo: string;
   public listado: string;
 
@@ -13,5 +13,21 @@ export class VideojuegoComponent {
     this.titulo = "Componente de videojuegos";
     this.listado = "Listado de juegos";
     console.log("Se ha cargado el componente: videojuego.component.ts.")
+  }
+
+  ngOnInit(): void { //se ejecuta solo nada mas arrancar la pagina
+    console.log("OnInit ejecutado");
+  }
+
+  ngDoCheck(): void { //cada vez que se cambia el codigo,se carga, como al clicar,un evento,etc
+    console.log("DoCheck ejecutado");
+  }
+
+  ngOnDestroy(): void { //solo se ejecuta al eliminar el componente
+    console.log("OnDestroy ejecutado");
+  }
+
+  cambiarTitulo(){
+    this.titulo= "Nuevo titulo del componente";
   }
 }
