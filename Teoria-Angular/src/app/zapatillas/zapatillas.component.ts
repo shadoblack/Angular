7751c2fb3091b5b87deb0af1870228bf105fd775
EdgarRB <37355506+shadoblack@users.noreply.github.com>
2022-05-25 11:54:59 +0200

    directivas ng

--- Teoria-Angular/src/app/zapatillas/zapatillas.component.ts
+++ Teoria-Angular/src/app/zapatillas/zapatillas.component.ts
@@ -9,8 +9,12 @@ export class ZapatillasComponent implements OnInit{
   public titulo: string = 'Componente de zapatillas';
 
   public zapatillas: Array<Zapatilla>;
+  public marcas: String[];
+  public color: String;
 
   constructor(){
+    this.color ='yellow'
+    this.marcas = new Array;
     this.zapatillas = [
       new Zapatilla('Nike Fly','Nike','blanco',80,true),
       new Zapatilla('Nike excavator','Nike','brown',180,false),
@@ -21,6 +25,18 @@ export class ZapatillasComponent implements OnInit{
 
   ngOnInit(){
     console.log(this.zapatillas);
+    this.getMarcas()
 
+  }
+
+  getMarcas(){
+    this.zapatillas.forEach((zapatillas,index)=>{
+      if(this.marcas.indexOf(zapatillas.marca)<0){
+        this.marcas.push(zapatillas.marca);
+
+      }
+      console.log(index);
+    });
+    console.log(this.marcas);
   }
 }
